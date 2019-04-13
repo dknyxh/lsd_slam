dff2d2173d306de7591396f34b34b7a4ed902085
Kenan <dknyxh@gmail.com>
2019-04-13 17:28:17 -0400

    Fixed problem with point cloud and depth

--- lsd_slam_core/src/IOWrapper/OpenCV/ImageDisplay_OpenCV.cpp
+++ lsd_slam_core/src/IOWrapper/OpenCV/ImageDisplay_OpenCV.cpp
@@ -70,14 +70,18 @@ void displayThreadLoop()
 				}
 			}
 			cv::imshow(displayQueue.back().name, displayQueue.back().img);
+			cv::waitKey(1);
 			displayQueue.pop_back();
+
 		}
 	}
 	cv::destroyAllWindows();
 	openWindows.clear();
 
 	printf("ended image display thread!\n");
+	
 }
+
 void makeDisplayThread()
 {
 	imageThreadKeepRunning = true;
@@ -116,13 +120,13 @@ void displayImage(const char* windowName, const cv::Mat& image, bool autoSize)
 
 int waitKey(int milliseconds)
 {
-	return cv::waitKey(milliseconds);
+	return 1;
 }
 
 int waitKeyNoConsume(int milliseconds)
 {
 	// Cannot implement this with OpenCV functions.
-	return cv::waitKey(milliseconds);
+	return 1;
 }
 
 void closeAllWindows()
